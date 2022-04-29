6b2f1b90f1e8d00e8854387b96708f350f719de3
Erik Dodge <dodgees@users.noreply.github.com>
2022-04-28 22:03:41 -0500

    Get tiny hack in place!!!

--- hacks/models.py
+++ hacks/models.py
@@ -1,3 +1,5 @@
+import random
+
 from django.db import models
 
 
@@ -35,6 +37,22 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
-    # client = models.ForeignKey(Client, on_delete=models.SET_DEFAULT, default='VanillaJS')
-    # api = models.ForeignKey(Api, on_delete=models.SET_DEFAULT, default='None')
-    # persistence = models.ForeignKey(Persistence, on_delete=models.SET_DEFAULT, default='None')
+
+
+class Hack(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.DO_NOTHING)
+    client = models.ForeignKey(Client, on_delete=models.DO_NOTHING)
+    api = models.ForeignKey(Api, on_delete=models.DO_NOTHING)
+    persistence = models.ForeignKey(Persistence, on_delete=models.DO_NOTHING)
+
+    @staticmethod
+    def get_tiny_hack():
+        # if project_size:
+        #     project = Project.objects.filter(size=project_size)[0]
+        # else:
+        #     project = Project.objects.all()[0]
+        project = random.choice(Project.objects.all())
+        client = random.choice(Client.objects.all())
+        api = random.choice(Api.objects.all())
+        persistence = random.choice(Persistence.objects.all())
+        return Hack(project=project, client=client, api=api, persistence=persistence)
--- hacks/views.py
+++ hacks/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+from .models import Hack
+
+
+def index(request):
+    hack = Hack.get_tiny_hack()
+    template = loader.get_template('hacks/index.html')
+    context = {
+        'hack': hack,
+    }
+    return HttpResponse(template.render(context, request))
